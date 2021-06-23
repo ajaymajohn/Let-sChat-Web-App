431cfddfcccd31d8c54689d3c6c59172d814b72c
ajaymajohn <78007276+ajaymajohn@users.noreply.github.com>
2021-06-23 21:08:02 +0530

    Add files via upload

--- Let'sChat Web App_room.js
+++ Let'sChat Web App_room.js
@@ -16,20 +16,20 @@ var firebaseConfig = {
           room_name=document.getElementById("room_name").value;
           firebase.database().ref("/").child(room_name).update({purpose:"adding roomname"});
           localStorage.setItem("room_name",room_name);
-          window.location="kwitter_page.html";
+          window.location="Let'sChatWebApp_page.html  ";
     }
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-       row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
-       document.getElementById("output").innerHTML+=row;
-      });});}
+    function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+      row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
+     });});}
 getData();
 function redirectToRoomName(name){
-      localStorage.setItem("room_name",name);
-      window.location="Let'sChat Web App_page.html";
+     localStorage.setItem("room_name",name);
+     window.location="Let'sChatWebApp_page.html";
 }
 function logout(){
-      localStorage.removeItem("user_name");
-      localStorage.removeItem("room_name");
-      window.location="index.html";
+     localStorage.removeItem("user_name");
+     localStorage.removeItem("room_name");
+     window.location="index.html";
 }
